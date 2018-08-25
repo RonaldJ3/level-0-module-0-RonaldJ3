2c1723e37725529f2b0e1842c5ab8d659e477fc0
RonaldJ3 <league@WorthyMini-2.local>
2018-08-25 11:32:26 -0700

    I finished Greeter. KnockKnock, and Morning Zombie

---
+++ src/KnockKnock.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Knock knock.");
+		JOptionPane.showInputDialog("Ice cream.");
+		JOptionPane.showMessageDialog(null, "Ice cream if you don't let me in now.");
+	}
+}
---
+++ src/MoringZomibe.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class MoringZomibe {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "wake up");
+		JOptionPane.showMessageDialog(null, "eat breakfast");
+		JOptionPane.showMessageDialog(null, "brush my teeth");
+		JOptionPane.showMessageDialog(null, "brush my hair");
+	}
+}
